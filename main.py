751aade65b9f09fe50d8d54eb20f65a5da9448cf
MrVelasquezz <smyrnijoswest51@gmail.com>
2022-09-09 18:30:25 +0300

    upd

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 import pyautogui as pya
 from pynput import mouse
+from colorama import Fore
+from colorama import Style
+import keyboard as kb
 import re
 import time
 
-pos1, pos2, entered = [], [], 0
+pos1, pos2, entered, pix = [], [], 0, ''
 
 def get_pos(x, y, btn, pressed):
     global pos1
     global pos2
     global entered
+    global pix
 
     if len(pos1) == 0 and entered == 0: 
         pos1 = [x, y]
         entered = 1
     if len(pos2) == 0 and entered == 0:
         pos2 = [x, y]
+        pix = pya.pixel(x, y)
         entered = 1
 
-print('''
-███████╗░█████╗░░██████╗██╗░░░██╗  ░█████╗░██╗░░██╗███████╗██╗░░██╗███████╗██████╗░
+print(f'''
+{Fore.RED}███████╗░█████╗░░██████╗██╗░░░██╗  ░█████╗░██╗░░██╗███████╗██╗░░██╗███████╗██████╗░
 ██╔════╝██╔══██╗██╔════╝╚██╗░██╔╝  ██╔══██╗██║░░██║██╔════╝██║░██╔╝██╔════╝██╔══██╗
 █████╗░░███████║╚█████╗░░╚████╔╝░  ██║░░╚═╝███████║█████╗░░█████═╝░█████╗░░██████╔╝
 ██╔══╝░░██╔══██║░╚═══██╗░░╚██╔╝░░  ██║░░██╗██╔══██║██╔══╝░░██╔═██╗░██╔══╝░░██╔══██╗
 ███████╗██║░░██║██████╔╝░░░██║░░░  ╚█████╔╝██║░░██║███████╗██║░╚██╗███████╗██║░░██║
-╚══════╝╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░  ░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝''')
+╚══════╝╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░  ░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝{Style.RESET_ALL}''')
 
 file_name = input('Переместите файл в папку со скриптом, напишите сюда его название и нажмите Enter:::')
 
@@ -38,7 +43,7 @@ print(f'[+] Координаты поля: {pos1[0], pos1[1]}')
 entered = 0
 
 input('Наведите курсор на поле кнопку ввода и кликните левой кнопкой, после нажмите Enter:::')
-print(f'[+] Координаты кнопки: {pos2[0], pos2[1]}')
+print(f'[+] Координаты кнопки: {pos2[0], pos2[1], pix}')
 
 listener.stop()
 
@@ -54,7 +59,7 @@ if len(file_name) > 0:
     try:
          with open(f"{file_name}.txt", "r+") as f:
             parsed = f.read()
-            pattern = r"=+|\n"
+            pattern = r"={3,}|\n"
             if len(parsed)>0:
                 arr = []
                 result = re.split(pattern, parsed)
@@ -65,18 +70,30 @@ if len(file_name) > 0:
                 if len(arr) > 0: 
                     if len(pos1) == 2 and len(pos2) == 2:
                         for x in arr: 
-                            print('[+] Ввожу значение')
+                            print(f'[+] Ввожу значение {x}')
                             pya.moveTo(pos1[0], pos1[1])
-                            time.sleep(.2)
                             pya.click()
-                            time.sleep(.2)
-                            pya.write(x, interval=0.01)
-                            time.sleep(.2)
+                            kb.write(x)
                             pya.moveTo(pos2[0], pos2[1])
-                            time.sleep(.2)
                             pya.click()
-                            time.sleep(.2)
-                            print('[+] Кнопка нажата. Перезапуск цикла')
+                            pya.moveTo(pos1[0], pos1[1])
+                            pya.click()
+                            time.sleep(.5)
+                            const is_pix = pya.pixelMatchesColor(pos1[0], pos1[0], (pix[0], pix[0], pix[0]))
+                            if is_pix is True: 
+                                print(f'[+] {Fore.GREEN}Valid.{Style.RESET_ALL} Пароль {x} валиден')
+                                print(f'[+] Записываю валид в файл')
+                                try:
+                                    with open ('valid.txt', 'a') as v:
+                                        v.write(f'[+] Valid: {x}')
+                                        print(f'{Fore.GREEN}[+] Изменения успешно записаны в файл{Style.RESET_ALL}')
+                                except:
+                                    print(f'{Fore.RED}[-] Ошибка при записи файла{Style.RESET_ALL}')
+                            else:
+                                kb.press('ctrl+a')
+                                kb.release('ctrl+a')
+                                kb.press('backspace')
+                                print('[+] Кнопка нажата. Перезапуск цикла')
                 else:
                     print('[-] Произошла ошибка. Не удалось извлечь значения.')
                 #print(arr) 
@@ -84,5 +101,5 @@ if len(file_name) > 0:
                 print('[-] Файл пуст')
     except FileNotFoundError:
         print(f'[-] Файл {file_name} не найден')
-    except:
-       print('[-] Произошла ошибка')
+    #except:
+    #   print('[-] Произошла ошибка')
